f969bab437166fccb0d10f9572c3a58181f9cd7a
Moritz <moritz.mueller2@tu-dresden.de>
2022-09-20 11:16:54 +0200

    create guest user at connection

--- src/dbconnection.php
+++ src/dbconnection.php
@@ -8,12 +8,22 @@ function create_table_user(PDO $dbh) {
         );");
 }
 
+function insert_guest_user(PDO $dbh) {
+    $userid = 1;
+    $name = "guest";
+    // this should prevent anyone from logging in as user
+    $hash = "ßßßßßßßßßßßßßßßßßßßßßßß";
+    $dbh->query("INSERT INTO user (userid, name, hash) VALUES (
+        $userid, '$name', '$hash');");
+}
+
 $db_path = __DIR__ . "/../calendar.sqlite";
 try {
     $dbh = new PDO("sqlite:$db_path");
     // enable foreign keys support
     $dbh->exec("PRAGMA foreign_keys = ON;");
     create_table_user($dbh);
+    insert_guest_user($dbh);
 } catch (PDOException $e) {
     die("Error!: " . $e->getMessage() . "<br/>");
 }
