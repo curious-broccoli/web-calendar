1ca14733d186c3699afae42aaf8a26496e82977f
Moritz <moritz.mueller2@tu-dresden.de>
2022-10-18 11:18:59 +0200

    calendar header depending on locale/tz

--- html/calendar.php
+++ html/calendar.php
@@ -7,12 +7,34 @@
     <title>Calendar</title>
 </head>
 <body>
+    <?php
+    define('LOCALE', 'en_US');
+    define('TIMEZONE', 'Europe/London');
+    # get date (day, month, year) (later it can be set by user)
+    $selected_date = new DateTimeImmutable("now");
+    $date_formatter = new IntlDateFormatter(
+        LOCALE,
+        IntlDateFormatter::FULL,
+        IntlDateFormatter::FULL,
+        TIMEZONE,
+        IntlDateFormatter::GREGORIAN,
+    );
+    # first day is what weekday
+    # how many days cal_days_in_month
+    
+    ?>
     <div class="calendar">
-    <div class="calendar-header">Oktober 2017</div>
+    <div class="calendar-header">
+        <?php
+        $date_formatter->setPattern('MMMM Y');
+        echo $date_formatter->format($selected_date)
+        ?>
+    </div>
     <ol class="day-header">
     <?php
     require_once __DIR__ . "/../src/dbconnection.php";
 
+    // get name of all days (later depending on locale and timezone)
     $seconds_in_day = 60 * 60 * 24;
     $weekdays = array(
         'mon' => - 3 * $seconds_in_day,
@@ -22,17 +44,9 @@
         'fri' => $seconds_in_day,
         'sat' => 2 * $seconds_in_day,
         'sun' => 3 * $seconds_in_day);
-    $fmt = datefmt_create(
-        'en_US',
-        IntlDateFormatter::FULL,
-        IntlDateFormatter::FULL,
-        'Europe/London',
-        IntlDateFormatter::GREGORIAN,
-        'eeee'
-    );
-    // get name of all days depending on locale and timezone
+    $date_formatter->setPattern('eeee');
     foreach ($weekdays as $key => $day) {?>
-        <li><?=datefmt_format($fmt, $day)?></li>    
+        <li><?=$date_formatter->format($day)?></li>
     <?php
     }
     ?>
@@ -53,9 +67,7 @@
          <!-- <div><?=var_dump($row)?></div> -->
     <?php
     }
-
-    # first day is what weekday
-    # how many days
+    
     ?>
     </ol>
     </div>
