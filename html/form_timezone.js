71c4927cf26a11f27bdc751eec96369c0d760703
Moritz <moritz.mueller2@tu-dresden.de>
2023-01-02 14:15:30 +0100

    use JS to fill default datetime values for new event

--- html/form_timezone.js
+++ html/form_timezone.js
@@ -1,8 +1,10 @@
+// JS for the creating a new event page
+
 if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", addHandler);
+    document.addEventListener("DOMContentLoaded", handleTimezones);
 }
 else {
-    addHandler();
+    handleTimezones();
 }
 
 function getUtcInputElement(dateElement, timeElement, valueName) {
@@ -10,24 +12,45 @@ function getUtcInputElement(dateElement, timeElement, valueName) {
     const dateTimeElement = document.createElement("input");
     dateTimeElement.name = valueName;
     dateTimeElement.value = dateTime;
-    //MAKE IT HIDDEN
     return dateTimeElement;
 }
 
-function addHandler() {
+function roundToFullHour(hoursToAdd = 1) {
+    let d = new Date();
+    d.setHours(d.getHours() + hoursToAdd);
+    d.setMinutes(0);
+    return d;
+}
+
+function handleTimezones() {
     const form = document.querySelector('form');
     const dateStart = document.querySelector('#date_start');
     const timeStart = document.querySelector('#time_start');
     const dateEnd = document.querySelector('#date_end');
     const timeEnd = document.querySelector('#time_end');
+
+    // if it is e.g. 15:59 and changes to 16:00 between the 1st and 2nd time
+    // this function is called then it will result in the default end time
+    // being 2 hours later
+    const startDefault = roundToFullHour();
+    const endDefault = roundToFullHour(2);
+    // Swedish locale easily sets it to the ISO 8601 format
+    // which is used by the "date" input element
+    const locale = "sv";
+    dateStart.value = startDefault.toLocaleDateString(locale);
+    dateEnd.value = endDefault.toLocaleDateString(locale);
+    const options = { timeStyle: "short" };
+    timeStart.value = startDefault.toLocaleTimeString(locale, options);
+    timeEnd.value = endDefault.toLocaleTimeString(locale, options);
+
     // submit event is fired only if the automatic HTML form validation was successful
     form.addEventListener('submit', () => {
         const start = getUtcInputElement(dateStart, timeStart, "datetime_start");
         const end = getUtcInputElement(dateEnd, timeEnd, "datetime_end");
         // make new elements for new timestamps
         // and prevent the local time field values from being submitted
-        // because it feels cleaner
-        // but it shows ugly textboxes for a short time before being submitted
+        // because they aren't needed
+        // but it flashes new input fields for a short time before being submitted (ugly)
         form.replaceChild(start, dateStart);
         timeStart.remove();
         form.replaceChild(end, dateEnd);
--- html/new_event.php
+++ html/new_event.php
@@ -7,23 +7,17 @@ include __DIR__ . "/../src/top.php";
 require_once __DIR__ . "/../src/flash_message.php";
 require_once __DIR__ . "/../src/dbconnection.php";
 session_start();
-
-$format = "Y-m-d H:00";
-$datetime_next_hour = date($format, strtotime("now + 1 hour "));
-$datetime_next_next_hour = date($format, strtotime($datetime_next_hour . "+ 1 hour "));
-$next_split = explode(" ", $datetime_next_hour);
-$next_next_split = explode(" ", $datetime_next_next_hour);
 ?>
 <h2>New event</h2>
 <form action="create_event.php" method="post">
     <input type="text" name="name" placeholder="Event name" required autofocus /><br />
     <input type="text" name="location" placeholder="Location" required /><br />
     Start<br />
-    <input type="date" id="date_start" name="date_start" required value="<?= $next_split[0] ?>" />
-    <input type="time" id="time_start" name="time_start" required value="<?= $next_split[1] ?>" /><br />
+    <input type="date" id="date_start" name="date_start" required />
+    <input type="time" id="time_start" name="time_start" required /><br />
     End<br />
-    <input type="date" id="date_end" name="date_end" required value="<?= $next_next_split[0] ?>" />
-    <input type="time" id="time_end" name="time_end" required value="<?= $next_next_split[1] ?>" /><br />
+    <input type="date" id="date_end" name="date_end" required />
+    <input type="time" id="time_end" name="time_end" required /><br />
     <textarea name="description" rows="5" cols="31" placeholder="Description"></textarea><br />
     <select name="series">
         <option value="">Event series</option>
